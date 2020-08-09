6cd34dfec18a3a04ee6454923a1b0ccd6c948077
Will Toll <will@wtoll.com>
2020-08-09 18:50:04 -0500

    Added static methods for the static properties associated with the nucleobases.
    Added static methods in addition to the already existing associated methods for the various static properties of each element in the nucleobases enum. Also redefined the already existing associated methods to act simply as a wrapper for the static methods.

--- src/lib.rs
+++ src/lib.rs
@@ -11,69 +11,85 @@ pub enum Nucleobase {
     Uracil,
 }
 
+pub fn is_purine(base: &Nucleobase) -> bool {
+    match base {
+        Adenine | Guanine => true,
+        Cytosine | Thymine | Uracil => false,
+    }
+}
+
+pub fn is_pyrimidine(base: &Nucleobase) -> bool {
+    match base {
+        Adenine | Guanine => false,
+        Cytosine | Thymine | Uracil => true,
+    }
+}
+
+pub fn is_ketone(base: &Nucleobase) -> bool {
+    match base {
+        Adenine | Cytosine => false,
+        Guanine | Thymine | Uracil => true,
+    }
+}
+
+pub fn is_amine(base: &Nucleobase) -> bool {
+    match base {
+        Adenine | Cytosine => true,
+        Guanine | Thymine | Uracil => false,
+    }
+}
+
+pub fn is_ribonucleotide_base(base: &Nucleobase) -> bool {
+    match base {
+        Adenine | Cytosine | Guanine | Uracil => true,
+        Thymine => false,
+    }
+}
+
+pub fn is_deoxyribonucleotide_base(base: &Nucleobase) -> bool {
+    match base {
+        Adenine | Cytosine | Guanine | Thymine => true,
+        Uracil => false
+    }
+}
+
+pub fn letter_code(base: &Nucleobase) -> char {
+    match base {
+        Adenine => 'A',
+        Cytosine => 'C',
+        Guanine => 'G',
+        Thymine => 'T',
+        Uracil => 'U',
+    }
+}
+
+pub fn from_letter_code(letter_code: &char) -> Option<Nucleobase> {
+    match letter_code {
+        'A' | 'a' => Some(Adenine),
+        'C' | 'c' => Some(Cytosine),
+        'G' | 'g' => Some(Guanine),
+        'T' | 't' => Some(Thymine),
+        'U' | 'u' => Some(Uracil),
+        _ => None
+    }
+}
+
 impl Nucleobase {
-    pub fn is_purine(&self) -> bool {
-        match self {
-            Adenine | Guanine => true,
-            Cytosine | Thymine | Uracil => false,
-        }
-    }
+    pub fn is_purine(&self) -> bool { is_purine(self) }
 
-    pub fn is_pyrimidine(&self) -> bool {
-        match self {
-            Adenine | Guanine => false,
-            Cytosine | Thymine | Uracil => true,
-        }
-    }
+    pub fn is_pyrimidine(&self) -> bool { is_pyrimidine(self) }
 
-    pub fn is_ketone(&self) -> bool {
-        match self {
-            Adenine | Cytosine => false,
-            Guanine | Thymine | Uracil => true,
-        }
-    }
+    pub fn is_ketone(&self) -> bool { is_ketone(self) }
 
-    pub fn is_amine(&self) -> bool {
-        match self {
-            Adenine | Cytosine => true,
-            Guanine | Thymine | Uracil => false,
-        }
-    }
+    pub fn is_amine(&self) -> bool { is_amine(self) }
 
-    pub fn is_ribonucleotide_base(&self) -> bool {
-        match self {
-            Adenine | Cytosine | Guanine | Uracil => true,
-            Thymine => false,
-        }
-    }
+    pub fn is_ribonucleotide_base(&self) -> bool { is_ribonucleotide_base(self) }
 
-    pub fn is_deoxyribonucleotide_base(&self) -> bool {
-        match self {
-            Adenine | Cytosine | Guanine | Thymine => true,
-            Uracil => false
-        }
-    }
+    pub fn is_deoxyribonucleotide_base(&self) -> bool { is_deoxyribonucleotide_base(self) }
 
-    pub fn letter_code(&self) -> char {
-        match self {
-            Adenine => 'A',
-            Cytosine => 'C',
-            Guanine => 'G',
-            Thymine => 'T',
-            Uracil => 'U',
-        }
-    }
+    pub fn letter_code(&self) -> char { letter_code(self) }
 
-    pub fn from_letter_code(letter_code: char) -> Option<Nucleobase> {
-        match letter_code {
-            'A' | 'a' => Some(Adenine),
-            'C' | 'c' => Some(Cytosine),
-            'G' | 'g' => Some(Guanine),
-            'T' | 't' => Some(Thymine),
-            'U' | 'u' => Some(Uracil),
-            _ => None
-        }
-    }
+    pub fn from_letter_code(letter_code: &char) -> Option<Nucleobase> { from_letter_code(letter_code) }
 }
 
 #[cfg(test)]
@@ -101,16 +117,16 @@ mod tests {
 
     #[test]
     fn correct_letter_codes() {
-        assert_eq!(Nucleobase::from_letter_code('a'), Some(Adenine));
-        assert_eq!(Nucleobase::from_letter_code('A'), Some(Adenine));
-        assert_eq!(Nucleobase::from_letter_code('c'), Some(Cytosine));
-        assert_eq!(Nucleobase::from_letter_code('C'), Some(Cytosine));
-        assert_eq!(Nucleobase::from_letter_code('g'), Some(Guanine));
-        assert_eq!(Nucleobase::from_letter_code('G'), Some(Guanine));
-        assert_eq!(Nucleobase::from_letter_code('t'), Some(Thymine));
-        assert_eq!(Nucleobase::from_letter_code('T'), Some(Thymine));
-        assert_eq!(Nucleobase::from_letter_code('u'), Some(Uracil));
-        assert_eq!(Nucleobase::from_letter_code('U'), Some(Uracil));
-        assert_eq!(Nucleobase::from_letter_code('n'), None);
+        assert_eq!(Nucleobase::from_letter_code(& 'a'), Some(Adenine));
+        assert_eq!(Nucleobase::from_letter_code(& 'A'), Some(Adenine));
+        assert_eq!(Nucleobase::from_letter_code(& 'c'), Some(Cytosine));
+        assert_eq!(Nucleobase::from_letter_code(& 'C'), Some(Cytosine));
+        assert_eq!(Nucleobase::from_letter_code(& 'g'), Some(Guanine));
+        assert_eq!(Nucleobase::from_letter_code(& 'G'), Some(Guanine));
+        assert_eq!(Nucleobase::from_letter_code(& 't'), Some(Thymine));
+        assert_eq!(Nucleobase::from_letter_code(& 'T'), Some(Thymine));
+        assert_eq!(Nucleobase::from_letter_code(& 'u'), Some(Uracil));
+        assert_eq!(Nucleobase::from_letter_code(& 'U'), Some(Uracil));
+        assert_eq!(Nucleobase::from_letter_code(& 'n'), None);
     }
 }
